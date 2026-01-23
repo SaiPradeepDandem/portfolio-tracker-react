b79ae574a53dd2a09311352213d1c63dd65f7352
SaiPradeepDandem <saipradeep.dandem@gmail.com>
2026-01-23 16:26:48 +1100

    Added initial text and styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css'
 function App() {
   
   return (
-    <>
-      <div>
-        Hello World !!
+    <div>
+      <div className="app-header-div">
+        <div className='title'>Portfolio Tracker</div>
+        <div className='creator'>Sai Pradeep Dandem</div>
       </div>
-    </>
+    </div>
   )
 }
 
