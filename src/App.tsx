fb201cc86e913090edaf5142804b255088867f59
SaiPradeepDandem <saipradeep.dandem@gmail.com>
2026-01-24 10:38:42 +1100

    Layout refinement

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,16 @@ import './App.css'
 function App() {
   
   return (
-    <div>
-      <div className="app-header-div">
+    <div className="app-root">
+      <div className="app-header">
         <div className='title'>Portfolio Tracker</div>
         <div className='creator'>Sai Pradeep Dandem</div>
+      </div>
+      <div className="app-menu">
+
+      </div>
+      <div className="app-content">
+          Coming soon...
       </div>
     </div>
   )
